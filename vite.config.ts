962ebadc4f8f54ff73090bccae2d75e31fb21398
mateuszciolkowski <matitek1234@gmail.com>
2026-04-23 09:58:05 +0200

    fix: use root base URL for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/curriculum_vitae/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
